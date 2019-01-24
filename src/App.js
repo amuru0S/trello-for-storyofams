e1459e13669ec8dae0635a3badff546fac05ef95
amuru0S <amuru.codais@gmail.com>
2019-01-24 14:40:36 +0100

    adding reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Header from './components/Header/Header';
+import BoardContainer from './components/Board/BoardContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Header />
+       <BoardContainer />
       </div>
     );
   }
